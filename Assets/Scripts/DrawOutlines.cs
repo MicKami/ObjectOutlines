38e0cba4e5e7f5accaccf14c3da82868dc807c9e
mickam <mich.kam97@gmail.com>
2017-09-17 01:43:07 +0200

    Added blur

--- Assets/Scripts/DrawOutlines.cs
+++ Assets/Scripts/DrawOutlines.cs
@@ -3,27 +3,47 @@ using UnityEngine.Rendering;
 
 public class DrawOutlines: MonoBehaviour
 {
+    [Range(0,16)]
     [SerializeField]
-    private Renderer[] targets;
+    private int blurIterations;
     [SerializeField]
     private Material outlineMaterial;
+    [SerializeField]
+    private Renderer[] targets;
 
+
+    private CommandBuffer commandBuffer;
     private Camera cam;
     private int _prePassID;
     private int _blurredID;
+    private int _tempID;
+    private Material blurMaterial;
 
 
     private void Awake()
     {
         cam = GetComponent<Camera>();
-        CommandBuffer commandBuffer = new CommandBuffer();
+        blurMaterial = new Material(Shader.Find("Hidden/Blur"));
+        commandBuffer = new CommandBuffer();
         commandBuffer.name = "Outlines";
         cam.AddCommandBuffer(CameraEvent.BeforeImageEffects, commandBuffer);
 
         _prePassID = Shader.PropertyToID("_OutlinePrepass");
         _blurredID = Shader.PropertyToID("_Blurred");
+        _tempID = Shader.PropertyToID("_Temp");
+        UpdateCommandBuffer();
+    }
 
-        commandBuffer.GetTemporaryRT(_prePassID, -1, -1, 16, FilterMode.Bilinear);
+    private void OnValidate()
+    {
+        UpdateCommandBuffer();
+    }
+
+    private void UpdateCommandBuffer()
+    {
+        if (commandBuffer == null) return;
+        commandBuffer.Clear();
+        commandBuffer.GetTemporaryRT(_prePassID, -1, -1, 0, FilterMode.Bilinear);
         commandBuffer.SetRenderTarget(_prePassID, BuiltinRenderTextureType.CurrentActive);
         commandBuffer.ClearRenderTarget(false, true, Color.black);
 
@@ -32,9 +52,19 @@ public class DrawOutlines: MonoBehaviour
             commandBuffer.DrawRenderer(targets[i], outlineMaterial);
         }
 
-        commandBuffer.GetTemporaryRT(_blurredID, -2, -2, 16, FilterMode.Bilinear);
+        commandBuffer.GetTemporaryRT(_blurredID, -2, -2, 0, FilterMode.Bilinear);
+        commandBuffer.GetTemporaryRT(_tempID, -2, -2, 0, FilterMode.Bilinear);
         commandBuffer.Blit(_prePassID, _blurredID);
 
+        for (int i = 0; i < blurIterations; i++)
+        {
+            commandBuffer.Blit(_blurredID, _tempID, blurMaterial, 0);
+            commandBuffer.Blit(_tempID, _blurredID, blurMaterial, 1);
+        }
+
         commandBuffer.Blit(_blurredID, BuiltinRenderTextureType.CameraTarget);
+        commandBuffer.ReleaseTemporaryRT(_prePassID);
+        commandBuffer.ReleaseTemporaryRT(_blurredID);
+        commandBuffer.ReleaseTemporaryRT(_tempID);
     }
 }
---
+++ Assets/Shaders/Blur.shader
@@ -0,0 +1,97 @@
+﻿Shader "Hidden/Blur"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_TexelSize;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 sum = tex2D(_MainTex, i.uv + float2(-_MainTex_TexelSize.x * 2, 0)) * 0.06136;
+				sum += tex2D(_MainTex, i.uv + float2(-_MainTex_TexelSize.x, 0)) *	0.24477;
+				sum += tex2D(_MainTex, i.uv) * 0.38774;
+				sum += tex2D(_MainTex, i.uv + float2(_MainTex_TexelSize.x, 0)) * 0.24477;
+				sum += tex2D(_MainTex, i.uv + float2(_MainTex_TexelSize.x * 2, 0)) * 0.06136;				
+
+				return sum;
+			}
+			ENDCG
+		}
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_TexelSize;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 sum = tex2D(_MainTex, i.uv + float2(0, -_MainTex_TexelSize.y * 2)) * 0.06136;
+				sum += tex2D(_MainTex, i.uv + float2(0, -_MainTex_TexelSize.y)) *	0.24477;
+				sum += tex2D(_MainTex, i.uv) * 0.38774;
+				sum += tex2D(_MainTex, i.uv + float2(0, _MainTex_TexelSize.y)) * 0.24477;
+				sum += tex2D(_MainTex, i.uv + float2(0, _MainTex_TexelSize.y * 2)) * 0.06136;				
+
+				return sum;
+			}
+			ENDCG
+		}
+	}
+}
